bfd8edf25df97d321bdb8dfb076252bd31ee5d73
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-09-15 14:44:50 +0500

    add example006

---
+++ example005_ifElse/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите имя пользователя: ");
+string user = Console.ReadLine();
+if(user.ToLower() == "маша") {
+    Console.WriteLine("Ура, это же Маша!");
+} else {
+    Console.WriteLine("Привет, " + user);
+}
