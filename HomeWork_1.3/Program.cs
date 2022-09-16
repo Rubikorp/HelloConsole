6f3a647ef1d5ee6111d7d592c2c361dd7957c3fd
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-09-16 13:20:18 +0500

    add three task

---
+++ HomeWork_1.3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+Console.Write("Введите число: ");
+int number = Int32.Parse(Console.ReadLine()); 
+
+if (number % 2 == 0) {
+    Console.WriteLine("Да");
+} else {
+    Console.WriteLine("Нет");
+}
