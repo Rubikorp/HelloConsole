a95e558c9d7e78edb14f067d8ed26fcb5bdb5c8d
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-09-16 13:09:02 +0500

    add two task

---
+++ HomeWork_1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите число a: ");
+int a = Int32.Parse(Console.ReadLine()); 
+Console.Write("Введите число b: ");
+int b = Int32.Parse(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = Int32.Parse(Console.ReadLine());
+
+int max = 0;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("max = " + max);
