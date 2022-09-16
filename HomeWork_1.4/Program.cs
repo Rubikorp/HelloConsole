975970d785151055d1dc40fa9290ea354700ac7c
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-09-16 13:27:34 +0500

    add 4 task

---
+++ HomeWork_1.4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число: ");
+int number = Int32.Parse(Console.ReadLine()); 
+int count = 0;
+
+while (count < number - 1) {
+    count += 2;
+    Console.Write(count + " ");
+}
