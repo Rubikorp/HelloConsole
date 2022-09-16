d6f502f2d33adf0b2125d813d05df4e4eb53f134
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-09-16 12:59:33 +0500

    add one task

---
+++ HomeWork_1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите число a");
+int a = Int32.Parse(Console.ReadLine()); //Readline возвращает строку, поэтому сделал так
+Console.WriteLine("Введите число b");
+int b = Int32.Parse(Console.ReadLine());
+int max = 0;
+
+if(a < b) {
+    max = b;
+} else {
+    max = a;
+}
+
+Console.WriteLine("max = " + max);
