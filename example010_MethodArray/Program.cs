540e5485df7b90b934e090676eaf520c3edf5640
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-09-17 15:39:35 +0500

    Array and function

---
+++ example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1, 2, 3, 31, 4, 56, 2, 12};
+
+int n = array.Length;
+int find = 2;
+
+int index = 0;
+
+while (index < n) {
+    if(array[index] == find) {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
