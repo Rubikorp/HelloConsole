e65146c66300965bf888f10c91c494811050e5d2
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-10-02 10:35:40 +0500

    add 5 home work

---
+++ HomeWork_5.1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.Write("Введите длину массива: ");
+int size = Int32.Parse(Console.ReadLine());
+int[] arr = new int[size];
+FillArrayRandomNumbers(arr);
+PrintArray(arr);
+int count = 0;
+
+for (int index = 0; index < arr.Length; index++) {
+    if (arr[index] % 2 == 0) {
+        count++;
+    }
+}
+
+Console.WriteLine($"всего {arr.Length} чисел, {count} из них чётные");
+
+void FillArrayRandomNumbers(int[] Arr)
+{
+    for(int i = 0; i < Arr.Length; i++)
+    {
+        Arr[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] Arr)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < Arr.Length; i++)
+    {
+        Console.Write(Arr[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
---
+++ HomeWork_5.3/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] arr = new double[size];
+FillArrayRandomarr(arr);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(arr);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int index = 0; index < arr.Length; index++)
+{
+    if (arr[index] > max)
+        {
+            max = arr[index];
+        }
+    if (arr[index] < min)
+        {
+            min = arr[index];
+        }
+}
+
+Console.WriteLine($"всего {arr.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayRandomarr(double[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] arr)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
