b6e8c1a2891e897f26518c1948b2af59d8e6a62b
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-10-07 18:28:04 +0500

    add HomeWork 6

---
+++ HomeWork_6.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите число: ");
+int count = 0;
+
+while(true) {
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number == 111) break;
+    if (number > 0) {
+        count++;
+    }
+}
+Console.WriteLine($"Кол-во чисел больших 0: {count}");
---
+++ HomeWork_6.2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.Write("k1 = ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k2 = ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b1 = ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b2 = ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"({x}; {y})");
