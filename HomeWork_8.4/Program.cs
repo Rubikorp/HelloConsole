3b1692313b9b9972105d2be864827394404238fc
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-10-11 18:57:22 +0500

    Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.

---
+++ HomeWork_8.4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+void PrintTwoArray(int[,] TwoArray)
+{
+    for (int i = 0; i < TwoArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < TwoArray.GetLength(1); j++)
+        {
+            Console.Write($"{TwoArray[i, j]}({i}, {j}) ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillTwoArray(int[,] TwoArray)
+{
+    for (int i = 0; i < TwoArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < TwoArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < TwoArray.GetLength(0); k++)
+            {
+                for (int m = 0; m < TwoArray.GetLength(1); m++)
+                {
+                    int count = new Random().Next(10, 99);
+                    if(count != TwoArray[k,m])
+                    {
+                        TwoArray[i,j] = count;
+                    }
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = new int[4,2];
+FillTwoArray(matrix);
+PrintTwoArray(matrix);
