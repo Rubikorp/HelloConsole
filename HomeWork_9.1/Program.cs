0babc1e1513d6c230396707de08462e5cd1a66a1
Ruslan Bikmetov <bik.big102rus@gmail.com>
2022-10-14 19:14:48 +0500

    Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.

---
+++ HomeWork_9.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int NumbersNatural(int count)
+{
+    Console.Write($"{count} ");
+    if(count == 1) return count = 1;
+    else return NumbersNatural(count - 1);
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NumbersNatural(number);
